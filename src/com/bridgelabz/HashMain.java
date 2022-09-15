54ad4a8ec8de08885e873202d482e4ab9f5ce774
saps999 <saptarshibiswasgang@gmail.com>
2022-09-16 00:00:14 +0530

    Finding frequency of Words

--- src/com/bridgelabz/HashMain.java
+++ src/com/bridgelabz/HashMain.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 public class HashMain {
     public static void main(String[] args) {
-
+        HashFunction func = new HashFunction("To be or not to be",4);
+        func.showTable();
     }
 }

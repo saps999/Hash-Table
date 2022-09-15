ccaf8fc318e5005fb5856e283f703dc51bae1692
saps999 <saptarshibiswasgang@gmail.com>
2022-09-16 00:04:47 +0530

    Finding Frequemcy Of Large Paragraph Phrase

--- src/com/bridgelabz/HashMain.java
+++ src/com/bridgelabz/HashMain.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class HashMain {
     public static void main(String[] args) {
-        HashFunction func = new HashFunction("To be or not to be",4);
+        HashFunction func = new HashFunction("Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations",4);
         func.showTable();
     }
 }

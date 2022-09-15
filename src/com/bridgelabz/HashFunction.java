23d4026ae1d8c7b80e3d2c913f070933b4867002
saps999 <saptarshibiswasgang@gmail.com>
2022-09-15 23:55:57 +0530

    Finding frequency of words

---
+++ src/com/bridgelabz/HashFunction.java
@@ -0,0 +1,61 @@
+package com.bridgelabz;
+
+public class HashFunction {
+    MyMapNode[] table;
+    String[] strArray;
+    int hashSize;
+
+    public HashFunction(String strArray, int hashSize) {
+
+        this.table = new MyMapNode[hashSize];
+        this.hashSize = hashSize;
+        this.strArray = strArray.split(" ");
+        hashTable();
+    }
+
+    private boolean isEmpty() {
+        for (MyMapNode element : table) {
+            if (element != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void hashTable() {
+        for (int i = 0; i < strArray.length; i++) {
+            int bucketIndex = Math.abs(strArray[i].hashCode() % hashSize);
+            if (table[bucketIndex] == null) {
+                table[bucketIndex] = new MyMapNode(strArray[i]);
+            } else {
+                MyMapNode currNode = table[bucketIndex];
+                while (currNode != null) {
+                    if (currNode.data.equals(strArray[i])) {
+                        currNode.frequency++;
+                        break;
+                    }
+                    if (currNode.next == null) {
+                        currNode.next = new MyMapNode(strArray[i]);
+                        break;
+                    }
+                    currNode = currNode.next;
+                }
+
+            }
+        }
+    }
+
+    public void showTable() {
+        if (isEmpty()) {
+            return;
+        } else {
+            for (MyMapNode element : table) {
+                MyMapNode currNode = element;
+                while (currNode != null) {
+                    System.out.println(currNode.data + " " + currNode.frequency);
+                    currNode = currNode.next;
+                }
+            }
+        }
+    }
+}
---
+++ src/com/bridgelabz/MyMapNode.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class MyMapNode {
+    String data;
+    int frequency;
+    MyMapNode next;
+
+    public MyMapNode(String data) {
+        this.data = data;
+        this.frequency = +1;
+        this.next = null;
+    }
+}
